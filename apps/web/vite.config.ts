dfd2a9247895155b53009901b675065d13711ea4
akawahuynh <akawahuynh3006@gmail.com>
2026-03-20 15:17:34 +0700

    add

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,15 +3,20 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['bits-ui'] // Buộc Vite pre-bundle package này
-	},
-	ssr: {
-		noExternal: ['bits-ui'] // Biên dịch package này cho SSR
-	},
-	server: {
-		fs: {
-			allow: ['../../']
-		}
-	}
+	// optimizeDeps: {
+	// 	include: ['bits-ui'], // Buộc Vite pre-bundle package này
+	// 	rolldownOptions:{
+	// 		experimental:{
+	// 			chunkOptimization:true
+	// 		}
+	// 	}
+	// },
+	// ssr: {
+	// 	noExternal: ['bits-ui'] // Biên dịch package này cho SSR
+	// },
+	// server: {
+	// 	fs: {
+	// 		allow: ['../../']
+	// 	}
+	// }
 });
--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,10 +1,13 @@
-import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-export default defineConfig({ plugins: [tailwindcss(), svelte()],optimizeDeps: {
-    include: ['bits-ui'] // Buộc Vite pre-bundle package này
-  },
-  ssr: {
-    noExternal: ['bits-ui'] // Biên dịch package này cho SSR
-  }, });
+export default defineConfig({
+	plugins: [tailwindcss(), svelte()],
+	// optimizeDeps: {
+	// 	include: ['bits-ui'] // Buộc Vite pre-bundle package này
+	// },
+	// ssr: {
+	// 	noExternal: ['bits-ui'] // Biên dịch package này cho SSR
+	// }
+});
